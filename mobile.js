ec1f3dd50e3a0db9bfad854c431eb3287c9b0624
softRahul10 <softrahul10@gmail.com>
2022-12-16 03:46:01 +0530

    Done

--- mobile.js
+++ mobile.js
@@ -1,10 +1,13 @@
 // Don't show anything if screen is smaller
 window.addEventListener('resize',()=>{
     const deviceWidth = window.innerWidth;
-    const body = document.body;
+    const body = document.querySelector('.content');
+    const msgUi = document.querySelector('.special-msg');
     if(deviceWidth < 1000) {
         body.style.display = 'none';
+        msgUi.style.display = 'block';
     }else {
         body.style.display = 'block';
+        msgUi.style.display = 'none';
     }
 })
